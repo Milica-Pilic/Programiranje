04c82ea2ea26bc29694fd2138ecdaca6a7ef63bd
Milica-Pilic <71880118+Milica-Pilic@users.noreply.github.com>
2021-02-25 13:59:15 +0100

    Add files via upload

---
+++ 2. Polugodiste/4. nedelja/4. nedelja 1. zadatak/4. nedelja 1. zadatak/4. nedelja 1. zadatak.cpp
@@ -0,0 +1,13 @@
+
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// Vidite nastavnice ja imam koronu i jako mi tesko da gledam u ekran a posebno da kucam 
+//kod posto za to mi je potrebna koncetracija koju trenutno nemam.
+//domaci cu buraditi kada se budem osecala malo bolje i budem u stanju gde mogu da ga uradim.
+//Hvala na razumevanju
